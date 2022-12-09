11e5ef6281da9b2b1fdcf1a60a40c37bdcdb88f3
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2022-12-09 21:27:39 +0530

    Some easy questions : - makeap , bullbear , creds.

--- BULLBEAR.cpp
+++ BULLBEAR.cpp
@@ -5,11 +5,11 @@ int main() {
 	// your code goes here
 	int t;
 	cin>>t;
-	for(int i=0;i<t;i++){
+	while(t--){
 	    int a,b;
 	    cin>>a>>b;
-	    if(a>b) cout<<"LOSS\n";
-	    else if(a==b) cout<<"NEUTRAL\n";
+	    if (a>b) cout<<"LOSS\n";
+	    else if (a==b) cout<<"NEUTRAL\n";
 	    else cout<<"PROFIT\n";
 	}
 	return 0;
