7453561a8c7d8da9f82e747da8c337d3cb833585
Shridhar1115 <116793581+Shridhar1115@users.noreply.github.com>
2022-12-13 20:35:21 +0530

    Add files via upload

---
+++ MARKSTW.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a,b;
+	cin>>a>>b;
+	a=a/2;
+	if(b==a) {
+	    cout<<"Yes\n";
+	}
+	else{
+	  cout<<"No\n";  
+	} 
+	return 0;
+}
---
+++ PARTY2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    if((a*b)<=c) cout<<"YES\n";
+	    else cout<<"NO\n";
+	}
+	return 0;
+}
