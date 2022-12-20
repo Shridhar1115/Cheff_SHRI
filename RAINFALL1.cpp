839e9ae7d9cfdab21e2c60b16bd59f4244e40e18
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2022-12-20 20:15:28 +0530

    rainfall and intest

--- RAINFALL1.cpp
+++ RAINFALL1.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n<3)   cout<<"LIGHT\n";
+        else if (n>=3 && n<7) cout<<"MODERATE\n";
+        else cout<<"HEAVY\n";
+    }
 	// your code goes here
-	int t;
-	cin>>t;
-	for(int i=0;i<t;i++){
-	    int a;
-	    cin>>a;
-	    if(a<3) cout<<"LIGHT\n";
-	    else if(a>=3 && a<7) cout<<"MODERATE\n";
-	    else if(a>=7) cout<<"HEAVY\n";
-	}
 	return 0;
 }
