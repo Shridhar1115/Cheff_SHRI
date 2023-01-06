dcf133928224dda331141cdaa834dfcf073e54d9
Shridhar1115 <116793581+Shridhar1115@users.noreply.github.com>
2023-01-06 23:07:27 +0530

    Add files via upload

--- INSTAGRAM.cpp
+++ INSTAGRAM.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
-    int t;
-    cin>>t;
-    while(t--){
-        int a,b;
-        cin>>a>>b;
-        if (a<b) cout<<"NO\n";
-        else if (a>(10*b)) cout<<"YES\n";
-        else cout<<"NO\n";
-    }
 	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    int x,y;
+	    cin>>x>>y;
+	    y=y*10;
+	    if(x>y) cout<<"YES\n";
+	    else cout<<"NO\n";
+	}
 	return 0;
 }
