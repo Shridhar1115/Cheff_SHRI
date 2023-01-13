229ba8c2e13c1b8a70b5ea8fe604d30034a52d76
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2023-01-13 19:25:02 +0530

    Add files via upload

---
+++ WATERCONS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if (n>=2000) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+	// your code goes here
+	return 0;
+}
