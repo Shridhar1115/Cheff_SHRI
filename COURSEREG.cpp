d455e2a3f94a66d241275e4f04246ccac1d85d71
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2023-01-16 22:38:15 +0530

    Add files via upload

--- COURSEREG.cpp
+++ COURSEREG.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 
 int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        c=a+c;
+        if (b>=c) cout<<"Yes\n";
+        else cout<<"No\n";
+    }
 	// your code goes here
-	int t;
-	cin>>t;
-	for(int k=0;k<t;k++){
-	    int a[3];
-	    for(int i=0;i<3;i++){
-	        cin>>a[i];
-	    }
-	    if((a[1]-a[2])>=a[0]) cout<<"Yes\n";
-	    else cout<<"No\n";
-	}
 	return 0;
 }
