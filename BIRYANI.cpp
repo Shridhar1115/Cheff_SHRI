5140c2b030690c07135a96f42ddb7b1d2606a2cc
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2023-01-18 21:33:32 +0530

    Add files via upload

---
+++ BIRYANI.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        cout<<a*b<<endl;
+    }
+	// your code goes here
+	return 0;
+}
