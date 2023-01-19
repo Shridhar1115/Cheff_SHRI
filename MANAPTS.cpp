05d37173d76122380d219b56e5ce665a6928fc00
Shridhar1115 <116793581+Shridhar1115@users.noreply.github.com>
2023-01-19 20:12:29 +0530

    Add files via upload

---
+++ MANAPTS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<(b/a)<<endl;
+	}
+	return 0;
+}
