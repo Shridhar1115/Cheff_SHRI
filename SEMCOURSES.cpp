f86808f9e99d0998738de43556273ebb3fffda0c
Shridhar1115 <116793581+Shridhar1115@users.noreply.github.com>
2023-01-21 18:18:16 +0530

    Add files via upload

---
+++ SEMCOURSES.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    cout<<(a*b*c)<<endl;
+	}
+	return 0;
+}
