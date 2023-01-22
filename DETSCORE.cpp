c7103dae4e66be48256a78a739065d4a2680f354
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2023-01-22 19:23:09 +0530

    4 eazy codes.

---
+++ DETSCORE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        a=a/10;
+        cout<<a*b<<endl;
+    }
+	// your code goes here
+	return 0;
+}
