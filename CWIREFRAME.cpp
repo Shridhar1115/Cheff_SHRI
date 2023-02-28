d7bfdf0fde46370c2f56c236b5024862ac647d7a
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2023-02-28 22:31:43 +0530

    eaziest of the eaziest...

---
+++ CWIREFRAME.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        cout<<c*((2*a) + (2*b))<<endl;
+    }
+	// your code goes here
+	return 0;
+}
---
+++ EMAILREM.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<a-b<<endl;
+	// your code goes here
+	return 0;
+}
