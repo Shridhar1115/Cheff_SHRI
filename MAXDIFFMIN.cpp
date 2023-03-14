1f067842bccfdd54af738063dd71878bd5facfa5
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2023-03-14 23:07:15 +0530

    Add files via upload

---
+++ MAXDIFFMIN.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+# define ll long long
+int main() {
+    ll t;
+    cin>>t;
+    while(t--){
+        ll a,b,c;
+        cin>>a>>b>>c;
+        cout<<c-a<<endl;
+    }
+	// your code goes here
+	return 0;
+}
---
+++ ONEMORE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if (n>24) cout<<"Yes\n";
+        else cout<<"No\n";
+    }
+	// your code goes here
+	return 0;
+}
