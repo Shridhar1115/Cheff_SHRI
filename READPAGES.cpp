28a22affeb351707c273025ebab06724d064e861
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2023-03-18 22:12:24 +0530

    intermediate level

--- READPAGES.cpp
+++ READPAGES.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,k;
+        cin>>n>>m>>k;
+        if ((m*k)>=n) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
 	// your code goes here
-	int t;
-	cin>>t;
-	while(t--){
-	    int a,b,c;
-	    cin>>a>>b>>c;
-	    if((b*c)>=a) cout<<"YES\n";
-	    else cout<<"NO\n";
-	}
 	return 0;
 }
