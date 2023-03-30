ae598a5037c20137020d3621d9f0ba8550336671
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2023-03-30 20:52:42 +0530

    Add files via upload

---
+++ CANDIVIDE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if (n%3==-0) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+	// your code goes here
+	return 0;
+}
