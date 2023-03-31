5cc08f3d120a2d242507620d8fa841e0b6d0f6ca
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2023-03-31 20:41:43 +0530

    Add files via upload

--- SEMCOURSES.cpp
+++ SEMCOURSES.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        cout<<a*b*c<<endl;
+    }
 	// your code goes here
-	int t;
-	cin>>t;
-	while(t--){
-	    int a,b,c;
-	    cin>>a>>b>>c;
-	    cout<<(a*b*c)<<endl;
-	}
 	return 0;
 }
